8999a2356b75269b920af80db83ac8e07a635196
Tharindu Nuwan Madhushanka <tharindunm94@gmail.com>
2024-03-21 15:38:57 +0530

    Create customers table and Create seeder to read csv to fill the table

--- database/migrations/2024_03_21_092216_create_customers_table.php
+++ database/migrations/2024_03_21_092216_create_customers_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
+            $table->string('acc_number');
+            $table->string('name');
             $table->timestamps();
         });
     }
--- database/seeders/CustomerSeeder.php
+++ database/seeders/CustomerSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Customer;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,21 @@ class CustomerSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Customer::truncate();
+        
+        $csvFile = fopen(base_path("database/data/customers.csv"), "r");
+
+        $firstLine = true;
+
+        while (($data = fgetcsv($csvFile, 1000, ",")) !== false) {
+            if (!$firstLine) {
+                Customer::create([
+                    'acc_number' => $data[0],
+                    'name' => $data[1]
+                ]);
+            }
+            $firstLine = false;
+        };
+        fclose($csvFile);
     }
 }
