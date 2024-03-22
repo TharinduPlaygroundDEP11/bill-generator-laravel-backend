3214d2ca35e55e62661fb84bb827ab5d7bbc2e2c
Tharindu Nuwan Madhushanka <tharindunm94@gmail.com>
2024-03-22 14:20:19 +0530

    Validate the meter reading value with the dates

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Customer;
+use App\Models\MeterReading;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CustomerController extends Controller
 {
-    //
+    public function get ($number) {
+        $data = ['number' => $number];
+
+        $validator = Validator::make($data, [
+            'number' => 'required|exists:customers,account_number'
+        ], [
+            'number.exists' => 'Account number does not exists!'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors'=>$validator->errors()], 422);
+        }
+
+        $validatedData = $validator->validated();
+
+        $customer = Customer::where('account_number', $validatedData['number'])->first();
+
+        $customerId = $customer->id;
+        $customerName = $customer->name;
+
+        $lastReading = MeterReading::where('customer_id', $customerId)->orderBy('');
+    }
 }
--- app/Http/Controllers/MeterReadingController.php
+++ app/Http/Controllers/MeterReadingController.php
@@ -10,8 +10,9 @@ use Illuminate\Support\Facades\Validator;
 
 class MeterReadingController extends Controller
 {
-    public function save(Request $request) {
-        Validator::extend('before_today', function($attribute, $value, $parameters, $validator) {
+    public function save(Request $request)
+    {
+        Validator::extend('before_today', function ($attribute, $value, $parameters, $validator) {
             $today = now()->startOfDay();
             $selectedDate = Carbon::parse($value)->startOfDay();
             return $selectedDate->lt($today);
@@ -38,6 +39,24 @@ class MeterReadingController extends Controller
         $customer = Customer::where('account_number', $validatedData['number'])->first();
 
         $customerId = $customer->id;
+
+        $previousReading = MeterReading::where('customer_id', $customerId)
+            ->whereDate('date', '<', $validatedData['date'])
+            ->orderByDesc('date')
+            ->first();
+
+        $nextReading = MeterReading::where('customer_id', $customerId)
+        ->whereDate('date', '>', $validatedData['date'])
+        ->orderBy('date')
+        ->first();
+
+        if ($previousReading && $validatedData['value'] < $previousReading->value) {
+            return response()->json(['error' => 'Reading value should be larger than the previous value'], 422);
+        }
+
+        if ($nextReading && $validatedData['value'] > $nextReading->value) {
+            return response()->json(['error' => 'Reading value should be less than the next value'], 422);
+        }
 
         $meterReading = MeterReading::create([
             'customer_id' => $customerId,
