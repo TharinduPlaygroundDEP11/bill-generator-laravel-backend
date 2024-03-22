502fea3654da361261ca2cad6d3a7f43ffc32d7f
Tharindu Nuwan Madhushanka <tharindunm94@gmail.com>
2024-03-22 17:55:11 +0530

    Complete the implementation of the CustomerController get()

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Customer;
 use App\Models\MeterReading;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
 class CustomerController extends Controller
