b5f318435dbe0c7592c1e50e29cce51d3d6c7347
Tharindu Nuwan Madhushanka <tharindunm94@gmail.com>
2024-03-26 14:11:07 +0530

    Fix issue today can not enter

--- app/Http/Controllers/MeterReadingController.php
+++ app/Http/Controllers/MeterReadingController.php
@@ -12,10 +12,10 @@ class MeterReadingController extends Controller
 {
     public function save(Request $request)
     {
-        Validator::extend('before_today', function ($attribute, $value, $parameters, $validator) {
+        Validator::extend('not_future_date', function ($attribute, $value, $parameters, $validator) {
             $today = now()->startOfDay();
             $selectedDate = Carbon::parse($value)->startOfDay();
-            return $selectedDate->lt($today);
+            return $selectedDate->lte($today);
         });
 
         Validator::extend('unique_date', function ($attribute, $value, $parameters, $validator) {
@@ -34,12 +34,12 @@ class MeterReadingController extends Controller
 
         $validator = Validator::make($request->all(), [
             'number' => 'required|exists:customers,account_number',
-            'date' => 'required|date|before_today|unique_date',
+            'date' => 'required|date|not_future_date|unique_date',
             'value' => 'required|integer|gte:0'
         ], [
             'number.exists' => 'Account number does not exists',
             'date.required' => 'Reading date can not be empty',
-            'date.before_today' => 'Reading date can not be a future date',
+            'date.not_future_date' => 'Reading date can not be a future date',
             'date.unique_date' => 'Already added a reading to this date',
             'value.required' => 'Reading value can not be empty',
             'value.integer' => 'Reading value should be an number',
